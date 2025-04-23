b2db1ee12d6954419a31bf0149e6b3d9bb233895
Basel Anani <basel.anani@finlocker.com>
2025-04-22 20:47:01 -0700

    fix button

--- app/components/AddToCartButton.tsx
+++ app/components/AddToCartButton.tsx
@@ -1,5 +1,6 @@
 import {type FetcherWithComponents} from '@remix-run/react';
 import {CartForm, type OptimisticCartLineInput} from '@shopify/hydrogen';
+import styles from './AddToCartButton.module.css';
 
 export function AddToCartButton({
   analytics,
@@ -25,16 +26,7 @@ export function AddToCartButton({
           />
           <button
             type="submit"
-            style={{
-              width: '100%',
-              fontFamily: 'Bodoni',
-              border: '1px solid black', // Black rectangular border
-              padding: '10px 20px', // Adjust padding for better appearance
-              backgroundColor: 'transparent', // Keep background neutral
-              cursor: 'pointer', // Change cursor on hover
-              fontSize: '1rem', // Optional: Adjust font size
-              transition: 'all 0.3s ease-in-out', // Smooth hover effect
-            }}
+            className={styles.button}
             onClick={onClick}
             disabled={disabled ?? fetcher.state !== 'idle'}
           >
